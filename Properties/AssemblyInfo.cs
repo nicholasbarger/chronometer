b0a03103cab7ed92f96d10973e10cf559bad6067
Nicholas Barger <nicholas@nicholasbarger.com>
2015-05-06 13:05:22 -0400

    Adding nuget spec and package creation

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Chronometer")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A datetimepicker that supports timezones.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Nicholas Barger")]
 [assembly: AssemblyProduct("Chronometer")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
